078988e9a2988aa460b30906d5a597a223dec3e7
Vinicius Soares <viniciussoares72943@gmail.com>
2023-01-23 18:52:10 -0300

    refatora controller para filtrar requisição get

--- app/backend/src/database/controllers/matchers.controller.ts
+++ app/backend/src/database/controllers/matchers.controller.ts
@@ -1,14 +1,28 @@
 import { Request, Response } from 'express';
 import * as MatchersService from '../services/matches.service';
 
-const getListMatches = async (req: Request, res: Response) => {
+const getSeachInProgressOrlistAll = async (req: Request, res: Response) => {
   try {
-    const matches = await MatchersService.getListMatches();
+    const { inProgress } = req.query;
 
-    return res.status(200).json(matches);
+    if (!inProgress) {
+      const matches = await MatchersService.getListMatches();
+
+      return res.status(200).json(matches);
+    }
+
+    let value = 0;
+
+    if (inProgress === 'true') {
+      value = 1;
+    }
+
+    const getInProgress = await MatchersService.getSeachInProgress(value as number);
+
+    return res.status(200).json(getInProgress);
   } catch (err:unknown) {
     return res.status(500).json({ message: 'Erro interno', error: err });
   }
 };
 
-export default getListMatches;
+export default getSeachInProgressOrlistAll;
