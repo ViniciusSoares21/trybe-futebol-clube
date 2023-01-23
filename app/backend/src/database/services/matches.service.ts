2e7411b38cbffad57adfb94a876482867bee1048
Vinicius Soares <viniciussoares72943@gmail.com>
2023-01-23 18:51:12 -0300

    cria func de requisição por qury string

--- app/backend/src/database/services/matches.service.ts
+++ app/backend/src/database/services/matches.service.ts
@@ -1,6 +1,14 @@
 import Matches from '../models/Matchers';
 import Teams from '../models/Teams';
 
+const getSeachInProgress = async (value:number) => {
+  const inProgress = await Matches.findAll({ where: { inProgress: value },
+    include: [{ model: Teams, as: 'homeTeam', attributes: { exclude: ['id'] } },
+      { model: Teams, as: 'awayTeam', attributes: { exclude: ['id'] } }] });
+
+  return inProgress;
+};
+
 const getListMatches = async () => {
   const matches = await Matches.findAll({ include: [
     { model: Teams, as: 'homeTeam', attributes: { exclude: ['id'] } },
@@ -9,6 +17,4 @@ const getListMatches = async () => {
   return matches;
 };
 
-const get = () => '';
-
-export { getListMatches, get };
+export { getListMatches, getSeachInProgress };
