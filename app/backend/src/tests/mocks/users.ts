d4ef0481150da6d99bd834e269903e84d7016e39
Vinicius Soares <viniciussoares72943@gmail.com>
2023-01-25 14:05:43 -0300

    inicia test no endpoint /login/validate

--- app/backend/src/tests/mocks/users.ts
+++ app/backend/src/tests/mocks/users.ts
@@ -2,4 +2,8 @@ const responseLoginVaid = {
   role: 'admin'
 }
 
-export { responseLoginVaid }
+const mockToken = {
+  token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwicm9sZSI6ImFkbWluIiwiaWF0IjoxNjU0NTI3MTg5fQ.XS_9AA82iNoiVaASi0NtJpqOQ_gHSHhxrpIdigiT-fc'
+}
+
+export { responseLoginVaid, mockToken }
---
+++ app/backend/src/tests/user.me.test.ts
@@ -0,0 +1,31 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+
+import * as UserService from '../database/services/user.service';
+import { responseLoginVaid, mockToken } from './mocks/users';
+import User from '../database/models/Users';
+
+import { app } from '../app';
+
+chai.use(chaiHttp);
+const { expect } = chai;
+
+describe('Teste endpoint /login/validate, com sucesso', () => {
+  beforeEach(() => {
+    sinon.stub(UserService, 'getByUserEmail').resolves(responseLoginVaid as unknown as User );
+  });
+
+  afterEach(() => {
+    (UserService.getByUserEmail as sinon.SinonStub).restore();
+  });
+
+  it('Retornar um objeto contendo a role do user e enviar status 200', async () => {
+    const { status, body } = await chai.request(app).set("Authorization", mockToken).get('/login/validate');
+
+    expect(status).to.be.equal(200);
+    //expect(body).to.be.an('object');
+    expect(body).to.be.deep.equal(responseLoginVaid);
+  });
+});
