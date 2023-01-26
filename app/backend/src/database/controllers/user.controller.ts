018464670755bfe2b3903b45e5fcc48d33852bb9
Vinicius Soares <viniciussoares72943@gmail.com>
2023-01-26 12:45:49 -0300

    refatora arquivos users para POO

--- app/backend/src/database/controllers/user.controller.ts
+++ app/backend/src/database/controllers/user.controller.ts
@@ -1,18 +1,17 @@
 import { Request, Response } from 'express';
-import * as UserService from '../services/user.service';
-// import UserService from '../services/user.service';
+import UserService from '../services/user.service';
 
-const getTypeUser = async (req: Request, res: Response) => {
-  try {
-    const { data } = req.body.userData;
-    // const userService = new UserService();
-    // const user = await userService.getByUserEmail(data.email);
-    const user = await UserService.getByUserEmail(data.email);
+export default class UserController {
+  constructor(private _UserService = new UserService()) {}
 
-    return res.status(200).json({ role: user?.role });
-  } catch (err:unknown) {
-    return res.status(500).json({ message: 'Erro interno', error: err });
-  }
-};
+  public getTypeUser = async (req: Request, res: Response) => {
+    try {
+      const { data } = req.body.userData;
 
-export default getTypeUser;
+      const user = await this._UserService.getByUserEmail(data.email);
+      return res.status(200).json({ role: user?.role });
+    } catch (err:unknown) {
+      return res.status(500).json({ message: 'Erro interno', error: err });
+    }
+  };
+}
--- app/backend/src/database/routers/user.router.ts
+++ app/backend/src/database/routers/user.router.ts
@@ -1,12 +1,14 @@
 import { Router } from 'express';
 import login from '../controllers/login';
-import getTypeUser from '../controllers/user.controller';
+import UserController from '../controllers/user.controller';
 import auth from '../middleware/auth';
 import validateLogin from '../middleware/validateLogin';
+
+const userController = new UserController();
 
 const userRouter = Router();
 
 userRouter.post('/login', validateLogin, login);
-userRouter.get('/login/validate', auth, getTypeUser);
+userRouter.get('/login/validate', auth, userController.getTypeUser);
 
 export default userRouter;
